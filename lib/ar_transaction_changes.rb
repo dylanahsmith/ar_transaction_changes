4fa13a09f12a7d623ffd2d2d45cc3416d279fd5c
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2013-09-15 02:46:11 -0400

    Add support for rails 4.
    Fixes #1

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -1,14 +1,16 @@
 require "ar_transaction_changes/version"
 
 module ArTransactionChanges
-  def create(*)
-    super.tap do |status|
+  create_or_update_suffix = ActiveRecord::VERSION::MAJOR <= 3 ? "" : "_record"
+
+  define_method :"create#{create_or_update_suffix}" do |*args|
+    super(*args).tap do |status|
       store_transaction_changed_attributes if status != false
     end
   end
 
-  def update(*)
-    super.tap do |status|
+  define_method :"update#{create_or_update_suffix}" do |*args|
+    super(*args).tap do |status|
       store_transaction_changed_attributes if status != false
     end
   end
