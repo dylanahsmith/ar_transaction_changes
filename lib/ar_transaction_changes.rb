69c295832fce6fd234fea218770c733805bc0d4f
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-05-08 12:22:26 -0400

    Use the more stable run_callbacks API method for forward compatiblity.

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -1,39 +1,16 @@
 require "ar_transaction_changes/version"
 
 module ArTransactionChanges
-  # rails 4.0.0: renamed create/update to create_record/update_record
-  # rails 4.0.6/4.1.2: prefixed create_record/update_record with an underscore
-  create_or_update_method_format = ["_%s_record", "%s_record", "%s"].detect do |format|
-    ["create", "update"].all? do |action|
-      method_name = format % action
-      ActiveRecord::Persistence.private_method_defined?(method_name.to_sym)
+  def run_callbacks(kind, &block)
+    ret = super
+    case kind.to_sym
+    when :create, :update
+      store_transaction_changed_attributes if ret != false
+    when :commit, :rollback
+      @transaction_changed_attributes = nil
     end
-  end or raise "Failed to find create/update record methods to monkey patch"
-  create_method_name = (create_or_update_method_format % "create").to_sym
-  update_method_name = (create_or_update_method_format % "update").to_sym
 
-  define_method(create_method_name) do |*args|
-    super(*args).tap do |status|
-      store_transaction_changed_attributes if status != false
-    end
-  end
-
-  define_method(update_method_name) do |*args|
-    super(*args).tap do |status|
-      store_transaction_changed_attributes if status != false
-    end
-  end
-
-  def committed!(*)
-    super
-  ensure
-    @transaction_changed_attributes = nil
-  end
-
-  def rolledback!(*)
-    super
-  ensure
-    @transaction_changed_attributes = nil
+    ret
   end
 
   def transaction_changed_attributes
