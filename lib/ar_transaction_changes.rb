65bcd492bcd0d5ea47a5bfa8885bd5957822e998
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-05-08 11:14:55 -0400

    Fix create and update method names that got prefixed with an underscore.

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -1,15 +1,24 @@
 require "ar_transaction_changes/version"
 
 module ArTransactionChanges
-  create_or_update_suffix = ActiveRecord::VERSION::MAJOR <= 3 ? "" : "_record"
+  create_or_update_method_format = if ActiveRecord::VERSION::MAJOR <= 3
+    create_method_name = :create
+    update_method_name = :update
+  elsif ActiveRecord::Persistence.private_method_defined?(:create_record)
+    create_method_name = :create_record
+    update_method_name = :update_record
+  else
+    create_method_name = :_create_record
+    update_method_name = :_update_record
+  end
 
-  define_method :"create#{create_or_update_suffix}" do |*args|
+  define_method(create_method_name) do |*args|
     super(*args).tap do |status|
       store_transaction_changed_attributes if status != false
     end
   end
 
-  define_method :"update#{create_or_update_suffix}" do |*args|
+  define_method(update_method_name) do |*args|
     super(*args).tap do |status|
       store_transaction_changed_attributes if status != false
     end
