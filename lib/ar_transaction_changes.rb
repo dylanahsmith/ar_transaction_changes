f4024f035eb96629bf6cb6ec844a652fbbdbbb05
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-12-02 11:54:15 -0500

    Support rails 4.2. Fixes issue #3
    In rails 4.2 ActiveRecord::Callbacks stopped calling run_callbacks and
    started calling the internal _run_#{kind}_callbacks methods. Unfortunately
    the internal changes in ActiveSupport::Callbacks prevented me from easily
    implementing this in a version independant way.

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -1,16 +1,42 @@
 require "ar_transaction_changes/version"
 
 module ArTransactionChanges
-  def run_callbacks(kind, &block)
-    ret = super
-    case kind.to_sym
-    when :create, :update
+  if (ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR >= 2) || ActiveRecord::VERSION::MAJOR > 4
+    def _run_create_callbacks
+      ret = super
       store_transaction_changed_attributes if ret != false
-    when :commit, :rollback
+      ret
+    end
+
+    def _run_update_callbacks
+      ret = super
+      store_transaction_changed_attributes if ret != false
+      ret
+    end
+
+    def _run_commit_callbacks
+      super
+    ensure
       @transaction_changed_attributes = nil
     end
 
-    ret
+    def _run_rollback_callbacks
+      super
+    ensure
+      @transaction_changed_attributes = nil
+    end
+  else
+    def run_callbacks(kind)
+      ret = super
+      case kind.to_sym
+      when :create, :update
+        store_transaction_changed_attributes if ret != false
+      when :commit, :rollback
+        @transaction_changed_attributes = nil
+      end
+
+      ret
+    end
   end
 
   def transaction_changed_attributes
