16e2a1e2ee456b12783c625a7507be7d361fdcaa
Lisa Ugray <lisa.ugray@shopify.com>
2016-08-04 15:03:04 -0400

    Re-add old version as deprecated

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -1,16 +1,34 @@
 require "ar_transaction_changes/version"
 
 module ArTransactionChanges
+  def _run_create_callbacks
+    ret = super
+    store_deprecated_transaction_changed_attributes if ret != false
+    ret
+  end
+
+  def _run_update_callbacks
+    ret = super
+    store_deprecated_transaction_changed_attributes if ret != false
+    ret
+  end
+
   def _run_commit_callbacks
     super
   ensure
+    @deprecated_transaction_changed_attributes = nil
     @transaction_changed_attributes = nil
   end
 
   def _run_rollback_callbacks
     super
   ensure
+    @deprecated_transaction_changed_attributes = nil
     @transaction_changed_attributes = nil
+  end
+
+  def deprecated_transaction_changed_attributes
+    changed_attributes.merge(@deprecated_transaction_changed_attributes ||= {})
   end
 
   def transaction_changed_attributes
@@ -25,4 +43,9 @@ module ArTransactionChanges
     end
     super
   end
+
+  def store_deprecated_transaction_changed_attributes
+    @deprecated_transaction_changed_attributes = deprecated_transaction_changed_attributes
+  end
+
 end
