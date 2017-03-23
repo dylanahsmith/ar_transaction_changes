6f15ec3ab5fad9ec28082df1d1e937f23862d4e6
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2017-03-22 20:12:19 -0400

    Remove deprecated methods
    Closes #13
    
    The primary use for this gem is from the identity cache gem and it
    doesn't need the deprecated behaviour.  With rails 5.1 identity
    cache will now get a lot of warnings when this deprecated behaviour
    is included in a model, even when it isn't used.
    
    Rather than supporting these deprecated methods in this gem, they can
    always be extracted by any app that actually needs them into a separate
    module to be included where needed (e.g. DeprecatedArTransactionChanges).
    So I am going to remove them before making another release.

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -1,34 +1,16 @@
 require "ar_transaction_changes/version"
 
 module ArTransactionChanges
-  def _run_create_callbacks
-    ret = super
-    store_deprecated_transaction_changed_attributes if ret != false
-    ret
-  end
-
-  def _run_update_callbacks
-    ret = super
-    store_deprecated_transaction_changed_attributes if ret != false
-    ret
-  end
-
   def _run_commit_callbacks
     super
   ensure
-    @deprecated_transaction_changed_attributes = nil
     @transaction_changed_attributes = nil
   end
 
   def _run_rollback_callbacks
     super
   ensure
-    @deprecated_transaction_changed_attributes = nil
     @transaction_changed_attributes = nil
-  end
-
-  def deprecated_transaction_changed_attributes
-    changed_attributes.merge(@deprecated_transaction_changed_attributes ||= {})
   end
 
   def transaction_changed_attributes
@@ -46,9 +28,4 @@ module ArTransactionChanges
     end
     ret
   end
-
-  def store_deprecated_transaction_changed_attributes
-    @deprecated_transaction_changed_attributes = deprecated_transaction_changed_attributes
-  end
-
 end
