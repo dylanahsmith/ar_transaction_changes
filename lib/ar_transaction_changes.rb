4fdb93f5549b292a87b8f44134c6f58f0c7c4dbf
Edouard Chin <edouard.chin@shopify.com>
2020-06-16 15:20:30 +0200

    `write_attribute` no longer delegates to `_write_attribute: (#28)
    - Since rails/rails@27a1ca2bfeda4298bbf44da17d07fac4147a4b1c,
      doing `model[:attr_name] = ...` will no longer go through
      `_write_attribute`

--- lib/ar_transaction_changes.rb
+++ lib/ar_transaction_changes.rb
@@ -19,9 +19,25 @@ module ArTransactionChanges
   end
 
   def _write_attribute(attr_name, value)
+    _store_transaction_changed_attributes(attr_name) do
+      super(attr_name, value)
+    end
+  end
+
+  if ActiveRecord.version >= Gem::Version.new('6.1.0.alpha')
+    def write_attribute(attr_name, value)
+      _store_transaction_changed_attributes(attr_name) do
+        super(attr_name, value)
+      end
+    end
+  end
+
+  private
+
+  def _store_transaction_changed_attributes(attr_name)
     attr_name = attr_name.to_s
     old_value = read_attribute(attr_name)
-    ret = super(attr_name, value)
+    ret = yield
     new_value = read_attribute(attr_name)
     unless transaction_changed_attributes.key?(attr_name) || new_value == old_value
       transaction_changed_attributes[attr_name] = old_value
